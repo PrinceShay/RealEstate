f39a7a33717c6767516b20052a4972b2b0d07c32
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-10-10 02:08:55 +0200

    removed line for debugging

--- src/app/lib/sanityClient.js
+++ src/app/lib/sanityClient.js
@@ -12,6 +12,6 @@ export const client = createClient({
 // Image URL Builder
 const builder = imageUrlBuilder(client)
 
-function urlFor(source) {
-  return builder.image(source)
-}
+// function urlFor(source) {
+//   return builder.image(source)
+// }
