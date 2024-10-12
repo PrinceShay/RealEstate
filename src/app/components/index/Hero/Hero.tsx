e766ad85c33d10e05d8819cee6175073a9774d28
Prince Shay <140750677+PrinceShay@users.noreply.github.com>
2024-10-12 19:02:29 +0200

    added skeleton loader for Search List

--- src/app/components/index/Hero/Hero.tsx
+++ src/app/components/index/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import HeroSearch from "./HeroSearch";
 function Hero() {
   return (
     <section className="h-screen p-16">
-      <div className="relative w-full h-full  rounded-xl flex items-end justify-center overflow-hidden">
+      <div className="relative w-full h-full  rounded-xl flex items-end justify-center">
         <div className=" p-24 relative w-full h-full flex flex-col items-center justify-end z-20">
           <div className=" flex flex-col text-center text-white mb-12">
             <h1 className="text-7xl font-medium mb-4">Ich bin eine Headline</h1>
@@ -17,15 +17,8 @@ function Hero() {
         <Image
           src={"/images/index/Hero.jpg"}
           alt="Hero"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            position: "absolute",
-          }}
+          fill
+          className="w-full h-full object-cover absolute rounded-2xl"
         />
       </div>
     </section>
